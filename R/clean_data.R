2de74a7357f41149088cad83fd64bca81620c302
Lise Vaudor <lise.vaudor@ens-lyon.fr>
2025-04-04 15:22:32 +0200

    add first vignette regarding cleaning

--- R/clean_data.R
+++ R/clean_data.R
@@ -3,7 +3,7 @@
 #' @param x the vector
 #' @param max_distinct the maximum number of distinct values to consider a column as free text. Defaults to 20.
 #' @export
-is_free_text=function(x,max_distinct=20){
+is_free_text=function(x,max_distinct=10){
   if(!is.character(x)){
     return(FALSE)
   }
@@ -18,28 +18,32 @@ is_free_text=function(x,max_distinct=20){
 #' @param x the vector
 #' @param possible_strings the strings provided in raw table which could possibly indicate a NA value
 #' @export
-clean_improper_NA=function(x, possible_strings=c("N/A",
+clean_improper_NAs=function(x, possible_strings=c("N/A",
                                                  "[Nn]on [Rr]enseigné",
                                                  "[Mm]anquant",
                                                  "[Uu]nknown",
                                                  "[Nn]ot provided",
                                                  "[Mm]issing")){
   for(i in 1:length(possible_strings)){
-    x[x==possible_strings[i]]=NA
+    x[stringr::str_detect(x,possible_strings[i])]=NA
   }
   #if an improper NA has made the column falsely recognized as character, convert it to numeric
-  if(all(is.na(as.numeric(x)))){return(x)}else{return(as.numeric(x))}
+  if(class(x)!="numeric"){
+    # Does this vector contain only number characters?
+    contains_only_numbers=any(!str_detect(x,"[^\\d.]*"),na.rm=TRUE)
+    if(contains_only_numbers){return(as.numeric(x))}else{return(x)}
+  }
   return(x)
 }
 
 #' Replace values improperly provided as a given string indicating missing value with NA
 #'
 #' @param x the vector
-#' @param acceptable_proportion the proportion of missing values above which the vector is considered as mainly missing
+#' @param threshold_proportion the proportion of missing values above which the vector is considered as mainly missing. Defaults to 0.33.
 #' @export
-is_mainly_missing=function(x,acceptable_proportion=0.33){
+is_mainly_missing=function(x,threshold_proportion=0.3333){
   proportion=length(which(is.na(x)))/length(x)
-  result=proportion>acceptable_proportion
+  result=proportion>threshold_proportion
   return(result)
 }
 
@@ -47,9 +51,9 @@ is_mainly_missing=function(x,acceptable_proportion=0.33){
 #'
 #' @param x the vector to optimize for running a random forest
 #' @param replace_NA_with the character string used to replace NA as category in categorical responses if not replaced by random values drawn from the observed distribution. Defaults to "Not provided"
-#' @param unpredictable_NA_proportion=0.2 the proportion of NAs that can be replaced by random values drawn from the observed distribution. Defaults to 0.2.
+#' @param threshold_missing_proportion the proportion of NAs that can be replaced by random values drawn from the observed distribution. Defaults to 0.3333.
 #' @export
-optimize_for_rf=function(x, replace_NA_with="Not provided", unpredictable_NA_proportion=0.2){
+optimize_for_rf=function(x, replace_NA_with="Not provided", threshold_missing_proportion=0.3333){
   # If x is numeric it is returned as is
   if(is.numeric(x)){return(x)}
   # If it is character, then it could be ordinal "0_not at all","1_a bit",..."5_very much" for instance
@@ -66,12 +70,18 @@ optimize_for_rf=function(x, replace_NA_with="Not provided", unpredictable_NA_pro
   ind=which(is.na(result))
   # If there are so many NAs that observed distribution is dubious and NAs cannot be filled randomly,
   # we consider that a NA is a category in itself
-  if(length(ind)>unpredictable_NA_proportion*length(result) & is.character(result)){
+  if(length(ind)>threshold_missing_proportion*length(result) & is.character(result)){
     result=case_when(is.na(result)~replace_NA_with,
                      TRUE~result)
     return(result)
   }
+  # or in case the variable is numeric, we keep the NA as is
+  if(length(ind)>threshold_missing_proportion*length(result) & is.numeric(result)){
+      return(result)
+  }
   #If there are few NAs, we replace them with random values drawn from observed distribution
-  result[ind]=sample(na.omit(result),length(ind))
+  if(length(ind)<=threshold_missing_proportion*length(result)){
+      result[ind]=sample(na.omit(result),length(ind), replace=TRUE)
+  }
   return(result)
 }
--- data-raw/build_data_icecream.R
+++ data-raw/build_data_icecream.R
@@ -7,10 +7,17 @@ sample_with_proportions=function(size,values,proportions){
   result=sample(values,size=size,replace=TRUE, prob=proportions)
   return(result)
 }
+randomly_introduce_values=function(x,value,proportion=0.1){
+  n=round(proportion*length(x))
+  ind=sample(1:length(x),n)
+  x[ind]=value
+  return(x)
+}
 
 icecream=tibble::tibble(
   id=1:n,
   age=round(runif(n,3,80)),
+  regular_eating=sample(c(rep("Oui", 42),rep("Non", 49)),size=n,replace=TRUE),
   genre=sample(c(rep("homme",42),rep("femme",49),rep("non binaire",8)),size=n,replace=TRUE)) %>%
   mutate(taille=case_when(genre=="homme" & age>18~ rnorm(n(),170,5),
                           genre=="femme" & age>18~ rnorm(n(),164,5),
@@ -39,6 +46,28 @@ icecream=tibble::tibble(
          creme_glacee_caramel=case_when(genre=="homme"~sample_with_proportions(n,values=values,proportions=c(30,20,20,20,20)),
                                         genre=="femme"~sample_with_proportions(n,values=values,proportions=c(20,20,30,30,50)),
                                         genre=="non binaire"~sample_with_proportions(n,values=values,proportions=c(20,20,20,20,20)))
-  )
+  ) %>%
+  mutate(comment=rep(NA,n)) %>%
+  mutate(regular_eating=randomly_introduce_values(regular_eating,value="Non renseigné",proportion=0.4)) %>%
+  mutate(across(c(age,genre,taille),~randomly_introduce_values(.x,value=NA,proportion=0.1))) %>%
+  mutate(across(starts_with("sorbet"),~randomly_introduce_values(.x,value="Non renseigné",proportion=0.1)))%>%
+  mutate(across(starts_with("creme_glacee"),~randomly_introduce_values(.x,value="Non renseigné",proportion=0.2))) %>%
+  mutate(creme_glacee_caramel=randomly_introduce_values(creme_glacee_caramel,value="Non renseigné",proportion=0.5))
+
+icecream$comment[33]="Moi j'adore les glaces mais c'est difficile de trouver de bons artisans de nos jours."
+icecream$comment[105]="Je ne sais pas si je vais pouvoir répondre à toutes les questions, je suis un peu fatigué aujourd'hui."
+icecream$comment[56]="J'essaie de réduire ma consommation suite au reportage de France 2 sur les glaces artisanales. Je ne sais pas si je vais y arriver, c'est tellement bon !"
+icecream$comment[28]="C'est quoi le but de votre enquête? c'est à ça que servent nos impôts j'y crois pas!"
+icecream$comment[10]="Pouet pouet camionnette!"
+icecream$comment[94]="Les meilleurs parfums ne sont pas cités: stracciatella, passion, noisette..."
+icecream$comment[97]="J'essaie de trouver des Nutriscore pas trop nases mais en glaces c'est la galère."
+icecream$comment[65]="L'offre de parfums en vegan c'est vraiment pas ça. Je suis déçu."
+icecream$comment[62]="Est-ce qu'on pourra recevoir les résultats de l'enquête? Je vous laisse mon mail: xxxx.xxxx@gmail.com"
+icecream$comment[61]="RAS"
+icecream$comment[34]="RAS"
+icecream$comment[56]="Bonne continuation, c'est un travail très important que vous réalisez!"
+
+
+
 usethis::use_data(icecream, overwrite=TRUE)
 
