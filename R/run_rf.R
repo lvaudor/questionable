35f5ce28c59ef6d8f74d8b6b549cee53aa10338e
Lise Vaudor <lise.vaudor@ens-lyon.fr>
2025-04-18 11:55:52 +0200

    adjust number of variables according to relative importance

--- R/run_rf.R
+++ R/run_rf.R
@@ -2,22 +2,27 @@
 #'
 #' @param data the data (optimized for running a rf)
 #' @param response the name of the response variable
+#' @param plot whether or not to plot the importance of predictors. Defaults to FALSE
 #' @export
 #' @examples
 #' # example code
 #' data(icecream)
 #' run_rf(icecream %>%
-#'           select(-id) %>%
-#'           mutate_all(optimize_for_rf),
-#'           response="sorbet_citron")
-run_rf=function(data,response=colnames(data)[1],plot=FALSE,clean_name=FALSE){
+#'           dplyr::select(-id) %>%
+#'           dplyr::mutate_all(optimize_for_rf),
+#'           response="sorbet_citron",
+#'           plot=TRUE)
+run_rf=function(data,
+                response=colnames(data)[1],
+                plot=FALSE,
+                min_importance=1){
   data=na.omit(data)
   datarf=dplyr::select(data,-dplyr::one_of(response))
   responserf=data %>%
     dplyr::select(dplyr::one_of(response)) %>%
     dplyr::pull()
   if(is.character(responserf)){responserf=as.factor(responserf)}
-  myrf=randomForest::randomForest(datarf,responserf,ntree=10000)
+  myrf=randomForest::randomForest(datarf,responserf,ntree=1000)
   impDF=data.frame(variable=rownames(myrf$importance),
                    importance=myrf$importance[,1],
                    stringsAsFactors=FALSE)
@@ -25,7 +30,16 @@ run_rf=function(data,response=colnames(data)[1],plot=FALSE,clean_name=FALSE){
   impDF$importance=impDF$importance/sum(impDF$importance)*100
   impDF=impDF %>% dplyr::arrange(desc(importance))
   if(plot==TRUE){
-    p=ggplot2::ggplot(impDF %>% head(20),
+    impDF_keep=impDF %>%
+      dplyr::filter(importance>=min_importance)
+    if(nrow(impDF_keep)==0){
+      impDF_keep=impDF %>%
+        dplyr::slice(1:5)
+      warning(paste0("All predictors have an importance <",
+                     min_importance,
+                     ". The top 5 predictors are plotted instead.")
+    }
+    p=ggplot2::ggplot(impDF_keep,
                       ggplot2::aes(x=variable, y=importance))+
       ggplot2::geom_point()+
       ggplot2::coord_flip()+
